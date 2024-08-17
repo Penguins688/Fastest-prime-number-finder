5ff32a8b83fdab20bbb2543abf9aa0eba4b2acd0
Eli <102241296+Penguins688@users.noreply.github.com>
2024-08-16 17:40:24 -0700

    Update main.cpp

--- find prime numbers/main.cpp
+++ find prime numbers/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <gmpxx.h>
+#include <vector>
+#include <pthread.h>
+#include <queue>
+
 using namespace std;
+
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+priority_queue<mpz_class, vector<mpz_class>, greater<mpz_class> > pq;
+mpz_class current_number;
 
 bool isPrime(const mpz_class& num) {
     if (num <= 1) return false;
@@ -11,9 +19,34 @@ bool isPrime(const mpz_class& num) {
     mpz_class i = 5;
     while (i * i <= num) {
         if (num % i == 0 || num % (i + 2) == 0) return false;
-        i+=6;
+        i += 6;
     }
     return true;
+}
+
+struct ThreadArgs {
+    mpz_class start;
+    int count;
+    ofstream* PrimeNumbers;
+};
+
+void* findPrimes(void* args) {
+    ThreadArgs* tArgs = static_cast<ThreadArgs*>(args);
+    mpz_class num = tArgs->start;
+    for (int i = 0; i < tArgs->count; i++) {
+        if (isPrime(num)) {
+            pthread_mutex_lock(&mtx);
+            pq.push(num);
+            while (!pq.empty() && pq.top() == current_number) {
+                *(tArgs->PrimeNumbers) << pq.top() << "\n";
+                pq.pop();
+                current_number++;
+            }
+            pthread_mutex_unlock(&mtx);
+        }
+        num++;
+    }
+    return NULL;
 }
 
 int main() {
@@ -25,10 +58,14 @@ int main() {
 
     string range;
     string start;
+    int numThreads;
+    
     cout << "Pick amount of iterations: ";
     cin >> range;
     cout << "Pick starting number: ";
     cin >> start;
+    cout << "Enter number of threads: ";
+    cin >> numThreads;
 
     int iterations;
     mpz_class startingNum;
@@ -44,16 +81,32 @@ int main() {
         return 1;
     }
 
-    mpz_class num = startingNum;
-
     PrimeNumbers << "\n";
-    for (int i = 0; i < iterations; i++) {
-        if (isPrime(num)) {
-            PrimeNumbers << num << "\n";
-        }
-        num++;
+
+    current_number = startingNum;
+
+    vector<pthread_t> threads(numThreads);
+    vector<ThreadArgs> threadArgs(numThreads);
+    int iterationsPerThread = iterations / numThreads;
+    int remainingIterations = iterations % numThreads;
+
+    for (int i = 0; i < numThreads; i++) {
+        threadArgs[i].count = iterationsPerThread + (i < remainingIterations ? 1 : 0);
+        threadArgs[i].start = startingNum + i * iterationsPerThread + (i < remainingIterations ? i : remainingIterations);
+        threadArgs[i].PrimeNumbers = &PrimeNumbers;
+        pthread_create(&threads[i], NULL, findPrimes, &threadArgs[i]);
+    }
+
+    for (int i = 0; i < numThreads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    while (!pq.empty()) {
+        PrimeNumbers << pq.top() << "\n";
+        pq.pop();
     }
 
     PrimeNumbers.close();
+    pthread_mutex_destroy(&mtx);
     return 0;
 }
